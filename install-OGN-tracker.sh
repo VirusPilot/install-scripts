719aaeff75afe4aebfd6cc0fd25c47fa803cd67a
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-03-09 12:54:26 +0100

    initial commit of OGN Tracker install script

---
+++ install-OGN-tracker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#set -x
+
+cd /root
+apt install python-pip libffi-dev libssl-dev -y
+pip install virtualenv
+
+cd
+git clone -b v4.1.1 --recursive https://github.com/espressif/esp-idf.git
+cd esp-idf
+./install.sh
+
+cd components
+git clone https://github.com/olikraus/u8g2.git
+
+cd
+git clone https://github.com/pjalocha/esp32-ogn-tracker.git
+cd esp32-ogn-tracker

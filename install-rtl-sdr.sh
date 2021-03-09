dce5730d30b948a5e8e280eda5874e0d9e9831d0
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-03-09 12:46:41 +0100

    Update install-rtl-sdr.sh

--- install-rtl-sdr.sh
+++ install-rtl-sdr.sh
@@ -9,5 +9,5 @@ apt install libusb-1.0-0-dev cmake git libtool
 git clone https://github.com/osmocom/rtl-sdr.git
 cd /root/rtl-sdr
 mkdir build && cd build
-cmake ../ -DINSTALL_UDEV_RULES=ON -DDETACH_KERNEL_DRIVER=ON
+cmake ../ -DENABLE_ZEROCOPY=0
 make && make install && ldconfig

a18ec1784f036597033d561f8242e099cde9d4d1
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-04-18 13:23:14 +0200

    Update install-dump1090-fa.sh

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -9,7 +9,7 @@ rm -rf dump1090
 git clone https://github.com/flightaware/dump1090.git
 cd dump1090
 dpkg-buildpackage -b --no-sign
-dpkg -i ../dump1090-fa_*.deb
+sudo dpkg -i ../dump1090-fa_*.deb
 
 echo
 echo "Now the dump1090-fa config file needs to be edited, e.g. --device-index and --ppm"

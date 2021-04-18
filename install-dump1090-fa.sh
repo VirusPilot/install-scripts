55ea167b5d1d74dd1c38d2304518fe01d9fd97a2
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-04-18 09:42:07 +0200

    Update install-dump1090-fa.sh

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 #set -x
 
-echo
-echo "Please note: install-rtl-sdr.sh needs to be run in advance"
-echo
-read -p "Press any key to continue or exit (Ctrl-C)"
-
-cd /root
-rm -rf /root/dump1090
+cd 
 
 apt install lighttpd build-essential debhelper librtlsdr-dev pkg-config dh-systemd libncurses5-dev libbladerf-dev libhackrf-dev liblimesuite-dev -y
 
+rm -rf dump1090
 git clone https://github.com/flightaware/dump1090.git
-cd /root/dump1090
+cd dump1090
 dpkg-buildpackage -b --no-sign
 dpkg -i ../dump1090-fa_*.deb
 
@@ -22,3 +17,4 @@ echo
 read -p "Press any key to continue"
 
 nano /etc/default/dump1090-fa
+cd

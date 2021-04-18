1dc029753fa81fe9c02e1160debcfc4c872bf3d6
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-04-18 13:23:40 +0200

    Update install-PlaneFinder.sh

--- install-PlaneFinder.sh
+++ install-PlaneFinder.sh
@@ -5,5 +5,5 @@ sudo apt update
 
 cd
 wget http://client.planefinder.net/pfclient_5.0.161_armhf.deb
-dpkg -i pfclient_5.0.161_armhf.deb
+sudo dpkg -i pfclient_5.0.161_armhf.deb
 rm -f pfclient_5.0.161_armhf.deb

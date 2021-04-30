c0a80218d8a2a18ecdff0940efc3706ae62111e1
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-04-30 16:32:00 +0200

    Create install-dump1090-fa-ogn2dump1090.sh

---
+++ install-dump1090-fa-ogn2dump1090.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#set -x
+
+sudo apt update
+sudo apt install lighttpd build-essential debhelper librtlsdr-dev pkg-config dh-systemd libncurses5-dev libbladerf-dev libhackrf-dev liblimesuite-dev -y
+
+cd
+rm -rf dump1090
+git clone https://github.com/VirusPilot/dump1090.git
+cd dump1090
+dpkg-buildpackage -b --no-sign
+sudo dpkg -i ../dump1090-fa_*.deb

e72a6934472896ec884ea3b5ae762e2519d76e5c
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-07-28 21:08:23 +0200

    Update install-rtl-sdr.sh
    add blacklist to prevent kernel modules claiming use of the SDR

--- install-rtl-sdr.sh
+++ install-rtl-sdr.sh
@@ -4,6 +4,14 @@
 sudo apt update
 sudo apt install libusb-1.0-0-dev cmake git libtool -y
 
+# prevent kernel modules claiming use of the SDR
+echo blacklist dvb_usb_rtl28xxu | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
+echo blacklist dvb_usb_v2 | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
+echo blacklist rtl2830 | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
+echo blacklist rtl2832 | sudo tee /etc/modprobe.d/rtl-sdr-blacklist.conf
+echo blacklist r820t | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
+echo blacklist rtl8xxxu | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
+
 cd
 rm -rf rtl-sdr
 git clone https://github.com/osmocom/rtl-sdr.git

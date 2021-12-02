6fc34836dea8d6c9b0f015ba58a841e45870a33a
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-12-02 17:38:19 +0100

    Update install-dump1090-fa.sh
    prepare for Bullseye arm64

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -2,11 +2,11 @@
 #set -x
 
 sudo apt update
-sudo apt install lighttpd build-essential debhelper pkg-config dh-systemd libncurses5-dev libbladerf-dev libhackrf-dev liblimesuite-dev -y
+sudo apt install librtlsdr-dev libusb-1.0-0-dev lighttpd build-essential debhelper pkg-config libncurses5-dev libbladerf-dev libhackrf-dev liblimesuite-dev -y
 
 cd
 rm -rf dump1090
-git clone https://github.com/flightaware/dump1090.git
+git clone -b dev https://github.com/flightaware/dump1090.git
 cd dump1090
 dpkg-buildpackage -b --no-sign
 sudo dpkg -i ../dump1090-fa_*.deb

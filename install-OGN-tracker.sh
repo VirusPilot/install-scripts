edeba48199675ebc7418bac81db3903be00c45fc
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2022-01-26 16:53:20 +0100

    minor update for OGN Tracker script

--- install-OGN-tracker.sh
+++ install-OGN-tracker.sh
@@ -2,11 +2,11 @@
 #set -x
 
 cd /root
-apt install python-pip libffi-dev libssl-dev -y
+apt install python3-pip libffi-dev libssl-dev -y
 pip install virtualenv
 
 cd
-git clone -b v4.1.1 --recursive https://github.com/espressif/esp-idf.git
+git clone -b v4.1.2 --recursive https://github.com/espressif/esp-idf.git
 cd esp-idf
 ./install.sh
 

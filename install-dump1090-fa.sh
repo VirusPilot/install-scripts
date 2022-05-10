0201c92f53ff96173b785c728056cfc946b2ad13
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2022-05-10 17:28:30 +0200

    Update install-dump1090-fa.sh
    - use latest "testing" librtlsdr libs from Debian
    - use my dump1090 fork as basis

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -2,13 +2,29 @@
 #set -x
 
 sudo apt update
-sudo apt install librtlsdr-dev libusb-1.0-0-dev lighttpd build-essential debhelper pkg-config libncurses5-dev libbladerf-dev libhackrf-dev liblimesuite-dev -y
+sudo apt install git libusb-1.0-0-dev lighttpd build-essential fakeroot debhelper pkg-config libncurses5-dev -y
+
+ARCH=$(arch)
+if [ $ARCH == aarch64 ]; then # arm64
+    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb
+    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_arm64.deb
+    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_arm64.deb
+else # armhf
+    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_armhf.deb
+    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_armhf.deb
+    wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_armhf.deb
+fi
+sudo dpkg -i *.deb
+rm -f *.deb
+sudo ldconfig
 
 cd
 rm -rf dump1090
-git clone https://github.com/flightaware/dump1090.git
+git clone https://github.com/VirusPilot/dump1090.git
 cd dump1090
-dpkg-buildpackage -b --no-sign
+./prepare-build.sh bullseye
+cd package-bullseye
+dpkg-buildpackage -b --no-sign --build-profiles=custom,rtlsdr
 sudo dpkg -i ../dump1090-fa_*.deb
 cd
 rm -rf dump1090

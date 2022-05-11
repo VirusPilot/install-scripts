b62dae774b793279753f31947f5537c53c7a9d05
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2022-05-11 09:34:54 +0200

    Update install-rtl-sdr.sh

--- install-rtl-sdr.sh
+++ install-rtl-sdr.sh
@@ -4,6 +4,10 @@
 sudo apt update
 sudo apt install libusb-1.0-0-dev cmake git libtool -y
 
+# remove previously (via the package manager) installed librtlsdr-dev
+sudo apt purge librtlsdr*
+sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr*
+
 # prevent kernel modules claiming use of the SDR
 echo blacklist dvb_usb_rtl28xxu | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
 echo blacklist dvb_usb_v2 | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf

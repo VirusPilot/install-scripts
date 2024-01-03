5829f9798f5dc0e6a18caa29a824341e65d07dc1
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2024-01-03 19:21:43 +0100

    switch to osmocom/rtl-sdr fork

--- install-rtl-sdr.sh
+++ install-rtl-sdr.sh
@@ -4,27 +4,21 @@
 sudo apt update
 sudo apt install libusb-1.0-0-dev cmake git libtool -y
 
-# remove previously (via the package manager) installed librtlsdr-dev
-sudo apt purge librtlsdr*
-sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr*
-
-# prevent kernel modules claiming use of the SDR
-echo blacklist dvb_usb_rtl28xxu | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
-echo blacklist dvb_usb_v2 | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
-echo blacklist rtl2830 | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
-echo blacklist rtl2832 | sudo tee /etc/modprobe.d/rtl-sdr-blacklist.conf
-echo blacklist r820t | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
-echo blacklist rtl8xxxu | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
-
-cd || exit
-rm -rf rtl-sdr
+cd
+apt purge ^librtlsdr
+rm -rf /root/rtl-sdr
+rm -rf /usr/lib/librtlsdr*
+rm -rf /usr/include/rtl-sdr*
+rm -rf /usr/local/lib/librtlsdr*
+rm -rf /usr/local/lib/aarch64-linux-gnu/librtlsdr*
+rm -rf /usr/local/include/rtl-sdr*
+rm -rf /usr/local/include/rtl_*
+rm -rf /usr/local/bin/rtl_*
 git clone https://github.com/VirusPilot/rtl-sdr.git
-cd rtl-sdr || exit
-mkdir build && cd build || exit
+cd rtl-sdr
+mkdir build
+cd build
 cmake ../ -DDETACH_KERNEL_DRIVER=ON -DINSTALL_UDEV_RULES=ON
 make
 sudo make install
 sudo ldconfig
-sudo make install-udev-rules
-cd || exit
-rm -rf rtl-sdr

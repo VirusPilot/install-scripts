df701629703c8b18e307ac477b83f812341b1331
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2024-01-06 13:06:32 +0100

    add missing package

--- install-rtl-sdr.sh
+++ install-rtl-sdr.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #set -x
 
+sudo apt update
+sudo apt install libusb-1.0-0-dev -y
+
 ARCH=$(getconf LONG_BIT)
 if [[ $ARCH -eq 64 ]]; then
     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb

c5d8ba8fc1bdfd070c57ecb679649f2047775399
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2024-01-06 14:38:32 +0100

    clean up and boot question added

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -10,9 +10,20 @@ cd dump1090
 sudo dpkg-buildpackage -b --no-sign --build-profiles=custom,rtlsdr
 cd ..
 sudo dpkg -i dump1090-fa_*.deb
+rm -f *.deb
+rm -f *.buildinfo
+rm -f *.changes
 
 echo
 echo "Now the dump1090-fa config file needs to be edited, e.g. RECEIVER_SERIAL=1090, RECEIVER_LAT=50.000 and RECEIVER_LON=10.000"
 echo
 read -p "Press any key to continue"
 sudo nano /etc/default/dump1090-fa
+
+echo
+read -t 1 -n 10000 discard
+read -p "Reboot now? [y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sudo reboot
+fi

e2ef09dbfdf75b31c4668f56ae0dfa6fc1c83c60
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2024-01-06 11:24:32 +0100

    reverting to install without Debian packaging

--- install-rtl-sdr.sh
+++ install-rtl-sdr.sh
@@ -2,16 +2,24 @@
 #set -x
 
 sudo apt update
-sudo apt install git libusb-1.0-0-dev debhelper -y
+sudo apt install git libusb-1.0-0-dev cmake -y
 
-# install librtlsdr Debian Package from https://github.com/osmocom/rtl-sdr fork
+# install librtlsdr from https://github.com/osmocom/rtl-sdr VirusPilot fork
 cd
 sudo apt purge ^librtlsdr
-sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_*
+sudo rm -rvf /usr/lib/librtlsdr*
+sudo rm -rvf /usr/include/rtl-sdr*
+sudo rm -rvf /usr/local/lib/librtlsdr*
+sudo rm -rvf /usr/local/lib/aarch64-linux-gnu/librtlsdr*
+sudo rm -rvf /usr/local/include/rtl-sdr*
+sudo rm -rvf /usr/local/include/rtl_*
+sudo rm -rvf /usr/local/bin/rtl_*
+sudo rm -rvf /lib/aarch64-linux-gnu/librtlsdr*
 git clone https://github.com/VirusPilot/rtl-sdr.git
 cd rtl-sdr
-sudo dpkg-buildpackage -b --no-sign
-cd ..
-sudo dpkg -i librtlsdr0_*.deb
-sudo dpkg -i librtlsdr-dev_*.deb
-sudo dpkg -i rtl-sdr_*.deb
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo ldconfig
